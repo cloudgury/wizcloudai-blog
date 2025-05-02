4f61cceee31f6bd33d35af5a69404fad466aa412
Leo <lg@allcloud.io>
2025-05-02 17:28:48 -0300

    new1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cloudgury.github.io/wizcloudai-blog",
+  site: "https://cloudgury.github.io",
+  base: "/wizcloudai-blog",
   integrations: [mdx(), sitemap()],
 });
